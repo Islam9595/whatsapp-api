0cce94ac4d439dbdcb2deb70523ae8cb873bed3f
Islam9595 <iemam@qurtobasolutions.com>
2023-11-05 12:22:27 +0200

    change venom-bot version

--- app/Http/Controllers/MainController.js
+++ app/Http/Controllers/MainController.js
@@ -49,7 +49,9 @@ exports.connections = async (req, res) => {
 }
 
 exports.sendMessage = async (req, res) => {
-  return null;
+  return res.json({
+    null
+  })
   const { connectionName, number, message ,url } = req.body || {};
   console.log(connectionName, number, message ,url)
   if (typeof number == 'undefined' || typeof message == 'undefined' || typeof url == 'undefined') {
