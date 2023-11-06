06356e8e58ca05865c11a97538200e259b909020
Islam9595 <iemam@qurtobasolutions.com>
2023-11-06 16:32:12 +0200

    return sendMessage

--- app/Http/Controllers/MainController.js
+++ app/Http/Controllers/MainController.js
@@ -49,10 +49,10 @@ exports.connections = async (req, res) => {
 }
 
 exports.sendMessage = async (req, res) => {
-  response=null
-  return res.json({
-    response
-  })
+  // response=null
+  // return res.json({
+  //   response
+  // })
   const { connectionName, number, message ,url } = req.body || {};
   console.log(connectionName, number, message ,url)
   if (typeof number == 'undefined' || typeof message == 'undefined' || typeof url == 'undefined') {
