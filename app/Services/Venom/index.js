cc5e491431152ca852b077eeca170e40a40fc328
Islam9595 <iemam@qurtobasolutions.com>
2023-12-01 21:37:41 +0200

    test new msg

--- app/Services/Venom/index.js
+++ app/Services/Venom/index.js
@@ -126,8 +126,6 @@ module.exports = new class {
           }else {
             resolve(res1)
           }
-          const res3= await client.sendText(`${number}@c.us`, 'Thank You.');
-          resolve(res3)
         }
         catch (error){
           reject(error)
